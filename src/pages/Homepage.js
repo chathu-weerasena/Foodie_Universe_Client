8ab3ff099cdd64430cce149be226fbb0efb25787
chathu-weerasena <chathuweerasena@gmail.com>
2022-10-06 20:50:31 +0200

    basic home page design

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,26 +1,51 @@
-import { Title } from "../styled"
-import { Link } from "react-router-dom"
-import { LinkWord } from "../styled"
-import styled from "styled-components"
+import styled from "styled-components";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 export const Homepage = () => {
-
   return (
     <Container>
-     <h3>Hello there 👋</h3>
-     <p>General information:</p>
-     <ul>
-      <li>Go to your backend and modify the config url</li>
-      <li>Make sure you clicked on the <b>use template</b> button on github</li>
-      <li>This template is using <a style={LinkWord} target="_blank" href="https://styled-components.com/">styled components</a>, you don't have to use it</li>
-      <li>You don't have to follow the folder structure, feel free to adapt to your own</li>
-      <li>Login and SignUp are already implemented</li>
-      <li>Modify this page to create your own homeepage</li>
-     </ul>
+      <Row xs={1} md={2} className="g-4">
+        {Array.from({ length: 1 }).map((_, idx) => (
+          <Col>
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://media-cdn.tripadvisor.com/media/photo-s/17/f5/39/f7/fooood-at-the-food-department.jpg"
+              />
+              <Card.Body>
+                <Card.Title>Photos</Card.Title>
+                <Card.Text>
+                  Welcome to the creative captures of your favorite food.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+      <Row xs={1} md={2} className="g-4">
+        {Array.from({ length: 1 }).map((_, idx) => (
+          <Col>
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://cdn.vox-cdn.com/thumbor/7HRjMUBf0ObMoA33zNPSYJEKsOE=/0x0:1600x1067/620x465/filters:focal(672x406:928x662):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57698831/51951042270_78ea1e8590_h.7.jpg"
+              />
+              <Card.Body>
+                <Card.Title>Restaurants</Card.Title>
+                <Card.Text>
+                  Share your dining experience with fellow Foodies!
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.div`
-  margin: 20px
-`
+  margin: 20px;
+`;
