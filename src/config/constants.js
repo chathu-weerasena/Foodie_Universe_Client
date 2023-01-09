537ad93eb097d51aed54b643eb9fc4e8b8d31be6
chathu-weerasena <chathuweerasena@gmail.com>
2023-01-09 16:10:15 +0100

    deployed backend

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,4 @@
 export const apiUrl =
-  process.env.API_URL || "https://foodie-universe-server.vercel.app/";
+  process.env.API_URL || "https://foodie-universe-server.vercel.app";
+//process.env.API_URL || "https://localhost:4000/";
 export const DEFAULT_MESSAGE_TIMEOUT = 2000;
