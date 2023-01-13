d67079e9d80462467bb0f4d86f14e289814ae95d
Chathu Weerasena <106069963+chathu-weerasena@users.noreply.github.com>
2023-01-13 13:29:51 +0100

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 export const apiUrl =
-  process.env.API_URL || "https://foodieuniverseserver.netlify.app";
+  process.env.API_URL || "https://foodieuniverse.onrender.com";
 //process.env.API_URL || "https://localhost:4000/";
 export const DEFAULT_MESSAGE_TIMEOUT = 2000;
