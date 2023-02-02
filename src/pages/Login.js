f019b830d0df2bdff737fd8f9e1e7be44eca47f0
Chathu Weerasena <106069963+chathu-weerasena@users.noreply.github.com>
2023-02-02 20:29:05 +0100

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,8 +11,8 @@ import { login } from "../store/user/thunks";
 import { selectToken } from "../store/user/selectors";
 
 export const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("chathu@chathu.com");
+  const [password, setPassword] = useState("chathu");
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
